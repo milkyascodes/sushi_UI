0a2b6132754ab21398307302c84a1dc365080e54
miki <milkyas.dev@gmail.com>
2022-03-29 19:57:25 +0300

    added CTA button and background image

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -30,24 +31,61 @@ class LandingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 60, vertical: 30),
-              color: Colors.green,
-              // width: 300,
-              child: Column(
-                children: [
-                  buildTitle('Baking Experience Like a Chef', 26),
-                  SizedBox(height: 20),
-                  buildDescription(
-                      'Let\'s make a delious dish with the best reciepe for the family',
-                      17)
-                ],
+        backgroundColor: Colors.transparent,
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 255, 255, 255),
+              // Color.fromARGB(255, 255, 255, 255),
+              Color.fromARGB(255, 0, 0, 0)
+            ],
+          )),
+          child: Stack(
+            children: [
+              Positioned(
+                  // top: 0,
+                  // left: 0,
+                  // right: 0,
+                  child: Image(
+                height: double.infinity,
+                colorBlendMode: BlendMode.exclusion,
+                image: AssetImage('assets/images/cake2.jpg'),
+                fit: BoxFit.cover,
+                // alignment: Alignment.topCenter,
+              )),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+                      // color: Colors.green,
+                      // width: 300,
+                      child: Column(
+                        children: [
+                          buildTitle('Baking Experience Like a Chef', 28, 1.4),
+                          SizedBox(height: 20),
+                          buildDescription(
+                              'Let\'s make a delious dish with the best reciepe for the family',
+                              17),
+                          SizedBox(height: 40),
+                          buildFAB(),
+                          SizedBox(height: 20),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/widget/widgets.dart
+++ lib/widget/widgets.dart
@@ -1,20 +1,48 @@
+// ignore_for_file: unnecessary_new, prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
-Widget buildTitle(String title, double size) {
-  return Text(
-    title,
-    textAlign: TextAlign.center,
-    style: TextStyle(
-      fontSize: size,
-      fontWeight: FontWeight.bold,
-    ),
-  );
+Widget buildTitle(String title, double size, double height) {
+  return Text(title,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+          fontSize: size,
+          fontWeight: FontWeight.w800,
+          height: height,
+          color: Colors.white));
 }
 
 Widget buildDescription(String description, double size) {
   return Text(
     description,
     textAlign: TextAlign.center,
-    style: TextStyle(fontSize: size, fontWeight: FontWeight.w200),
+    style: TextStyle(
+        fontSize: size, fontWeight: FontWeight.w300, color: Colors.white),
   );
+}
+
+Widget buildFAB() {
+  return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        fixedSize: Size(230, double.infinity),
+        primary: Colors.green,
+        padding: EdgeInsets.all(8),
+        shape: new RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(100.0),
+        ),
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            child: Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.green,
+            ),
+            backgroundColor: Colors.white,
+            radius: 28,
+          ),
+          Expanded(child: buildTitle('Get Started', 19, 1))
+        ],
+      ));
 }
